73d7e324c040bb9def1355e7156a0d0b7591471c
paulshnurenko <paulsnurenko@gmail.com>
2024-03-03 16:13:22 +0100

    Initial Commit

---
+++ TabBar/TabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarApp.swift
+//  TabBar
+//
+//  Created by Pavlo Shnurenko on 03.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
